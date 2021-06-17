ef7797dc6d1b226dac55282353ec09ab1042e58a
desty2k <wojciech.wentland@int.pl>
2021-06-17 12:43:54 +0200

    UPD: update examples to use jsonimporter

--- example/mss_example.py
+++ example/mss_example.py
@@ -1,23 +1,24 @@
-import io
+import json
 import paker
 import logging
 
+file = "mss.json"
+logging.basicConfig(level=logging.NOTSET)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-
-    # serialize and write module to file
-    serialized = paker.dump("mss")
+    # install mss using `pip install mss`
+    # serialize module
+    # serialized = paker.dump("mss")
+    # serialized = json.dumps(serialized, indent=4)
+    # with open(file, "w+") as f:
+    #     f.write(serialized)
 
     # now you can uninstall mss using `pip uninstall mss -y`
     # load package back from dump file
-    with open("mss.zip", "rb") as f:
-        zip_bytes = io.BytesIO(f.read())
+    with open(file, "r") as f:
+        loader = paker.load(json.loads(f.read()))
 
-    with paker.loads(zip_bytes.read()) as loader:
-        loader.load_module("mss")
-        import mss
-
-        print(dir(mss))
-        with mss.mss() as sct:
-            sct.shot()
+    import mss
+    print(dir(mss))
+    with mss.mss() as sct:
+        sct.shot()
--- example/psutil_example.py
+++ example/psutil_example.py
@@ -1,31 +1,32 @@
 """
-This example does not work on Windows.
-Psutil uses .pyd files.
+This example does not work.
+Psutil uses .pyd files in Windows.
 
 FIX?
 https://github.com/n1nj4sec/pymemimporter
 https://github.com/phuslu/zipextimporter
 """
 
-import io
+import json
 import paker
 import logging
 
+file = "psutil.json"
+logging.basicConfig(level=logging.NOTSET)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-
-    # serialize and write module to file
+    # install mss using `pip install psutil`
+    # serialize module
     serialized = paker.dump("psutil")
+    serialized = json.dumps(serialized, indent=4)
+    with open(file, "w+") as f:
+        f.write(serialized)
 
     # now you can uninstall mss using `pip uninstall psutil -y`
     # load package back from dump file
-    with open("psutil.zip", "rb") as f:
-        zip_bytes = io.BytesIO(f.read())
+    with open(file, "r") as f:
+        loader = paker.load(json.loads(f.read()))
 
-    with paker.loads(zip_bytes.read()) as loader:
-        loader.load_module("psutil")
-        import psutil
-
-        print(dir(psutil))
-        print("CPU count is {}".format(psutil.cpu_count()))
+    import psutil
+    print(dir(psutil))
+    print("CPU count is {}".format(psutil.cpu_count()))
