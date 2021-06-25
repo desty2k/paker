ff72767d978fb7d04aa70bd110a81daf036e8b65
desty2k <wojciech.wentland@int.pl>
2021-06-25 12:11:56 +0200

    ADD: import from dict example

---
+++ example/steam_example.py
@@ -0,0 +1,14 @@
+import paker
+import logging
+
+MODULE = {"somemodule": {"type": "module", "code": "fun = lambda x: x**2"}}
+logging.basicConfig(level=logging.NOTSET)
+
+if __name__ == '__main__':
+    with paker.loads(MODULE) as loader:
+        # somemodule will be available only in this context
+        from somemodule import fun
+        assert fun(2), 4
+        assert fun(5), 25
+        print("6**2 is {}".format(fun(6)))
+        print("It works!")
