de626e27f276b42effa7364c6f9a917da85fba8e
desty2k <wojciech.wentland@int.pl>
2021-07-11 11:47:05 +0200

    ADD: test for getting paker version (using paker -V)

---
+++ tests/test_version.py
@@ -0,0 +1,8 @@
+REGEX = r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+
+
+def test_print_version():
+    import re
+    from subprocess import run
+    version = "".join(run("python paker -V", capture_output=True).stdout.decode().splitlines()).replace("v", "")
+    assert re.match(REGEX, version) is not None
